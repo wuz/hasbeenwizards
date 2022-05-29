2e9007f562dae1a43dbb23539f131f3f910946a4
wuz <conlin@figurehr.com>
2022-05-29 17:50:57 -0400

    Add startplaying link

--- components/MainLayout.js
+++ components/MainLayout.js
@@ -24,10 +24,6 @@ const MainLayout = ({ children }) => {
     <Container>
       <header>
         <Nav>
-          <Link href="https://www.youtube.com/channel/UCE-CG2hIWTJrgKD99hSVaMw">
-            Youtube
-          </Link>
-          <Link href="https://twitter.com/hasbeenwizard">Twitter</Link>
           <Link href="/">
             <a>
               <Image height="50" width="50" src={logo} alt="Has Been Wizards" />
@@ -35,6 +31,13 @@ const MainLayout = ({ children }) => {
           </Link>
           <Link href="/about">About</Link>
           <Link href="/resources">Resources</Link>
+          <Link href="https://www.youtube.com/channel/UCE-CG2hIWTJrgKD99hSVaMw">
+            Youtube
+          </Link>
+          <Link href="https://twitter.com/hasbeenwizard">Twitter</Link>
+          <Link href="https://startplaying.games/gm/hasbeenwizard">
+            Join my table
+          </Link>
         </Nav>
       </header>
       <Main>{children}</Main>
